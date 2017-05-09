24a51fc9589e794f46078aff78245de5b03b6ac8
Sonny Sheth <sonny.sheth@gmail.com>
2017-05-09 09:23:40 -0300

    refactor to make camera component shareable

--- src/screens/PicturePage.js
+++ src/screens/PicturePage.js
@@ -1,55 +1,28 @@
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  Platform
-} from 'react-native';
 
 import Camera from '../components/Camera';
-
-const ImagePicker = require("react-native-image-picker");
 
 export default class PicturePage extends Component {
 
   constructor (props) {
     super(props);
+    this.onTapNext = this.onTapNext.bind(this);
+  }
+
+  onTapNext = () => {
+    alert('Tapping to go to next page');
   }
 
   render () {
 
     return (
-    <Camera
-      text = 'Take Photo'
-      next = 'Take Another'
-      nextStep = 'Next Step'
-    />
+      <Camera
+        text = 'Take Photo'
+        next = 'Take Another'
+        nextStepButtonText = 'Next Step'
+        nextStepAction = {this.onTapNext}
+      />
     )
   }
 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  button: {
-    backgroundColor: 'gray',
-    width: 150,
-    height: 50,
-    borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: 10
-  },
-  buttonText: {
-    color: 'white'
-  }
-
-});
